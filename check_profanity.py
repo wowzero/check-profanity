f0499b09712a18f241d0cbb7076ee7e4906f9918
yinquanlu <ZERO@MacBook-Air.local>
2017-06-22 23:33:25 +0800

    add some tips based on output

--- check_profanity.py
+++ check_profanity.py
@@ -7,10 +7,16 @@ def read_text():
     quotes.close()
     check_profanity(contents_of_file)
 
-def check_profanity(text_to_check):
+def check_profanity (text_to_check):
     connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" +text_to_check)
     output = connection.read()
-    print(output)
+    #print(output)
     connection.close()
+    if "true" in output:
+        print("Profanity Alter!!")
+    elif "flase" in output:
+        print("This document has no curse words.")
+    else:
+        print("could not scan the document properly")
     
 read_text()
