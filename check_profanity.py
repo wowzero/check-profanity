0e61295cac39207bbbb346b5d2acfeab336877ac
yinquanlu <ZERO@MacBook-Air.local>
2017-06-22 16:57:07 +0800

    This is used to check profanity in article by python.

---
+++ check_profanity.py
@@ -0,0 +1,16 @@
+import urllib
+
+def read_text():
+    quotes = open("/Users/ZERO/Desktop/python base/article.txt")
+    contents_of_file = quotes.read()
+    print contents_of_file
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" +text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+    
+read_text()
